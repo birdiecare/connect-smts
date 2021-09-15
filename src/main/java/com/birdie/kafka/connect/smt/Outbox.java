cf92fa4927218c6ae37dc68642d75e85f67746af
Samuel ROZE <samuel.roze@gmail.com>
2021-09-15 15:34:07 +0100

    Add an `Outbox` SMT with a failing test

---
+++ src/main/java/com/birdie/kafka/connect/smt/Outbox.java
@@ -0,0 +1,42 @@
+package com.birdie.kafka.connect.smt;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.connect.transforms.Transformation;
+import org.apache.kafka.connect.transforms.util.SimpleConfig;
+
+import java.util.Map;
+
+public class Outbox implements Transformation<SourceRecord> {
+    private interface ConfigName {
+        String TOPIC = "topic";
+    }
+
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+        .define(Outbox.ConfigName.TOPIC, ConfigDef.Type.STRING, ConfigDef.Importance.MEDIUM, "The name of the topic to send messages to.")
+    ;
+
+    private String targetTopic;
+
+    @java.lang.Override
+    public SourceRecord apply(SourceRecord sourceRecord) {
+        return sourceRecord;
+    }
+
+
+    @java.lang.Override
+    public void configure(Map<String, ?> props) {
+        final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
+
+        targetTopic = config.getString(ConfigName.TOPIC);
+    }
+
+    @java.lang.Override
+    public ConfigDef config() {
+        return CONFIG_DEF;
+    }
+
+    @java.lang.Override
+    public void close() {
+    }
+}
---
+++ src/test/java/com/birdie/kafka/connect/smt/OutboxTest.java
@@ -0,0 +1,52 @@
+package com.birdie.kafka.connect.smt;
+
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class OutboxTest {
+    @Test
+    public void sendsAMessageToTheCorrectTopicPartition() {
+        Outbox transformer = new Outbox();
+        transformer.configure(new HashMap<>() {{
+            put("topic", "caregivers.matches.v1");
+        }});
+
+        Schema schema = SchemaBuilder.struct()
+                .name("Value")
+                .field("event_key", SchemaBuilder.STRING_SCHEMA)
+                // Later: `partition_key`, SMT will figure out the number of partition of the target topic by itself
+                .field("partition_number", SchemaBuilder.INT32_SCHEMA)
+                .field("payload", SchemaBuilder.string().name("io.debezium.data.Json").optional().build())
+            .build();
+
+        Struct value = new Struct(schema);
+        value.put("event_key", "1234");
+        value.put("partition_number", 1);
+        value.put("payload", "[\"foo\", \"bar\"]");
+
+        SourceRecord record = new SourceRecord(
+                // Where it comes from
+                null, null,
+                // Where it is going
+                "a-database-name.public.the_database_table", 0,
+                // Key
+                SchemaBuilder.bytes().optional().build(),
+                "1234".getBytes(),
+                // Value
+                schema,
+                value
+        );
+
+        final SourceRecord transformedRecord = transformer.apply(record);
+
+        assertEquals("caregivers.matches.v1", transformedRecord.topic());
+        assertEquals(1, transformedRecord.kafkaPartition());
+    }
+}

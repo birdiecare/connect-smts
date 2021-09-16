093ca042edd3a004f83aadf8bbe6480ed5efc5d0
Samuel ROZE <samuel.roze@gmail.com>
2021-09-16 08:57:53 +0100

    Add headers to the Kafka record

--- src/main/java/com/birdie/kafka/connect/smt/Outbox.java
+++ src/main/java/com/birdie/kafka/connect/smt/Outbox.java
@@ -1,14 +1,26 @@
 package com.birdie.kafka.connect.smt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.transforms.Transformation;
 import org.apache.kafka.connect.transforms.util.SimpleConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Outbox implements Transformation<SourceRecord> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Outbox.class);
+
     private interface ConfigName {
         String TOPIC = "topic";
     }
@@ -18,12 +30,16 @@ public class Outbox implements Transformation<SourceRecord> {
     ;
 
     private String targetTopic;
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @java.lang.Override
     public SourceRecord apply(SourceRecord sourceRecord) {
         Struct value = (Struct) sourceRecord.value();
 
-        return sourceRecord.newRecord(
+        Schema valueSchema = sourceRecord.valueSchema();
+        Field headerField = valueSchema.field("headers");
+
+        SourceRecord transformed = sourceRecord.newRecord(
                 targetTopic,
                 value.getInt32("partition_number"),
                 sourceRecord.keySchema(),
@@ -32,6 +48,40 @@ public class Outbox implements Transformation<SourceRecord> {
                 value.get("payload"),
                 sourceRecord.timestamp()
         );
+
+        if (headerField != null) {
+            if (!headerField.schema().type().equals(Schema.Type.STRING)) {
+                LOGGER.error("Field 'headers' should be a string.");
+            } else {
+                Headers headers = sourceRecord.headers();
+
+                try {
+                    HashMap<String, String> headersToBeAdded = objectMapper.readValue(
+                            value.getString("headers"),
+                            new TypeReference<HashMap<String, String>>() {}
+                    );
+
+                    for (String key : headersToBeAdded.keySet()) {
+                        headers.add(key, headersToBeAdded.get(key), Schema.STRING_SCHEMA);
+                    }
+
+                    transformed = sourceRecord.newRecord(
+                            sourceRecord.topic(),
+                            sourceRecord.kafkaPartition(),
+                            sourceRecord.keySchema(),
+                            sourceRecord.key(),
+                            sourceRecord.valueSchema(),
+                            sourceRecord.value(),
+                            sourceRecord.timestamp(),
+                            headers
+                    );
+                } catch (JsonProcessingException e) {
+                    LOGGER.error("Could not decode headers.", e);
+                }
+            }
+        }
+
+        return transformed;
     }
 
 
--- src/test/java/com/birdie/kafka/connect/smt/OutboxTest.java
+++ src/test/java/com/birdie/kafka/connect/smt/OutboxTest.java
@@ -3,29 +3,36 @@ package com.birdie.kafka.connect.smt;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.header.Header;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.junit.Test;
 
 import java.util.HashMap;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class OutboxTest {
+    private final Schema schema = SchemaBuilder.struct()
+            .name("Value")
+            .field("key", SchemaBuilder.STRING_SCHEMA)
+            .field("partition_number", SchemaBuilder.INT32_SCHEMA)
+            .field("payload", SchemaBuilder.string().name("io.debezium.data.Json").optional().build())
+            .build();
+
+    private final Schema schemaWithHeaders = SchemaBuilder.struct()
+            .name("Value")
+            .field("key", SchemaBuilder.STRING_SCHEMA)
+            .field("partition_number", SchemaBuilder.INT32_SCHEMA)
+            .field("payload", SchemaBuilder.string().name("io.debezium.data.Json").optional().build())
+            .field("headers", SchemaBuilder.string().name("io.debezium.data.Json").optional().build())
+            .build();
+
     @Test
     public void sendsAMessageToTheCorrectTopicPartition() {
         Outbox transformer = new Outbox();
         transformer.configure(new HashMap<>() {{
             put("topic", "caregivers.matches.v1");
         }});
-
-        Schema schema = SchemaBuilder.struct()
-                .name("Value")
-                .field("key", SchemaBuilder.STRING_SCHEMA)
-                // Later: `partition_key`, SMT will figure out the number of partition of the target topic by itself
-                .field("partition_number", SchemaBuilder.INT32_SCHEMA)
-                .field("payload", SchemaBuilder.string().name("io.debezium.data.Json").optional().build())
-            .build();
 
         Struct value = new Struct(schema);
         value.put("key", "1234");
@@ -50,5 +57,42 @@ public class OutboxTest {
         assertEquals("caregivers.matches.v1", transformedRecord.topic());
         assertNotNull(transformedRecord.kafkaPartition());
         assertEquals("1", transformedRecord.kafkaPartition().toString());
+    }
+
+    @Test
+    public void sendsAMessageWithHeaders() {
+        Outbox transformer = new Outbox();
+        transformer.configure(new HashMap<>() {{
+            put("topic", "caregivers.matches.v1");
+        }});
+
+        Struct value = new Struct(schemaWithHeaders);
+        value.put("key", "1234");
+        value.put("partition_number", 1);
+        value.put("payload", "[\"foo\", \"bar\"]");
+        value.put("headers", "{\"agency_id\": \"1234\"}");
+
+        SourceRecord record = new SourceRecord(
+                // Where it comes from
+                null, null,
+                // Where it is going
+                "a-database-name.public.the_database_table", null,
+                // Key
+                SchemaBuilder.bytes().optional().build(),
+                "1234".getBytes(),
+                // Value
+                schemaWithHeaders,
+                value
+        );
+
+        final SourceRecord transformedRecord = transformer.apply(record);
+
+        HashMap<String, Object> headersAsHashmap = new HashMap<>();
+        for (Header header : transformedRecord.headers()) {
+            headersAsHashmap.put(header.key(), header.value());
+        }
+
+        assertTrue(headersAsHashmap.containsKey("agency_id"));
+        assertEquals("1234", headersAsHashmap.get("agency_id"));
     }
 }

9749e83ab0910c8267b3d38b68068d27a8acbe45
Arty <26905074+artysidorenko@users.noreply.github.com>
2021-09-16 09:12:16 +0100

    poc partitioning

--- src/main/java/com/birdie/kafka/connect/smt/Outbox.java
+++ src/main/java/com/birdie/kafka/connect/smt/Outbox.java
@@ -1,23 +1,48 @@
 package com.birdie.kafka.connect.smt;
 
 import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.common.utils.Utils;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.transforms.Transformation;
 import org.apache.kafka.connect.transforms.util.SimpleConfig;
-
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Outbox implements Transformation<SourceRecord> {
+    private ObjectMapper objectMapper = new ObjectMapper();
     private interface ConfigName {
         String TOPIC = "topic";
+        String AUTO_PARTITIONING = "auto-partitioning";
+        String NUM_TARGET_PARTITIONS = "num-target-partitions";
     }
 
     public static final ConfigDef CONFIG_DEF = new ConfigDef()
         .define(Outbox.ConfigName.TOPIC, ConfigDef.Type.STRING, ConfigDef.Importance.MEDIUM, "The name of the topic to send messages to.")
+        .define(Outbox.ConfigName.AUTO_PARTITIONING, ConfigDef.Type.BOOLEAN, false, ConfigDef.Importance.MEDIUM, "When true, SMT will generate new partition number using partition key (requires `num-target-partitions` config field and `partition_key` record field to be set).")
+        .define(Outbox.ConfigName.NUM_TARGET_PARTITIONS, ConfigDef.Type.INT, 0, ConfigDef.Importance.MEDIUM, "Number of partitions on the target topic")
     ;
 
     private String targetTopic;
+    private Boolean autoPartitioning;
+    private Integer targetPartitions;
+
+    @java.lang.Override
+    public void configure(Map<String, ?> props) {
+        final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
+
+        targetTopic = config.getString(ConfigName.TOPIC);
+        autoPartitioning = config.getBoolean(ConfigName.AUTO_PARTITIONING);
+        targetPartitions = config.getInt(ConfigName.NUM_TARGET_PARTITIONS);
+
+        if (autoPartitioning == true && targetPartitions == 0) {
+            throw new IllegalArgumentException("num-target-partitions is zero/null, when auto-partitioning is set to true");
+        }
+    }
 
     @java.lang.Override
     public SourceRecord apply(SourceRecord sourceRecord) {
@@ -25,7 +50,7 @@ public class Outbox implements Transformation<SourceRecord> {
 
         return sourceRecord.newRecord(
                 targetTopic,
-                value.getInt32("partition_number"),
+                getPartitionNumber(sourceRecord),
                 sourceRecord.keySchema(),
                 sourceRecord.key(),
                 sourceRecord.valueSchema().field("payload").schema(),
@@ -34,12 +59,53 @@ public class Outbox implements Transformation<SourceRecord> {
         );
     }
 
+    private Integer getPartitionNumber(SourceRecord sourceRecord) {
+        if (autoPartitioning == false) {
+            return getExplicitPartitionNumber(sourceRecord);
+        }
+        return getGeneratedPartitionNumber(sourceRecord);
+    }
 
-    @java.lang.Override
-    public void configure(Map<String, ?> props) {
-        final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
+    
+    private Integer getExplicitPartitionNumber(SourceRecord sourceRecord) {
+        Integer partition = ((Struct) sourceRecord.value()).getInt32("partition_number");
 
-        targetTopic = config.getString(ConfigName.TOPIC);
+        if (partition == null) {
+            throw new IllegalArgumentException("unable to find partition_number in source record");
+        }
+
+        return partition;
+    }
+    
+    private Integer getGeneratedPartitionNumber(SourceRecord sourceRecord) {
+        String partitionKey = ((Struct) sourceRecord.value()).getString("partition_key");
+        
+        if (partitionKey == null) {
+            throw new IllegalArgumentException("partition_key not set in source record");
+        }
+
+        String payloadString = ((Struct) sourceRecord.value()).getString("payload");
+
+        ObjectNode node;
+        try {
+            node = this.objectMapper.readValue(payloadString, ObjectNode.class);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("error reading partition_key in the source record");
+        }
+
+        // Support composite (comma-separated) partition keys
+        String partitionKeyValue = Arrays.stream(partitionKey.split(","))
+            .map(key -> {
+                if (node.has(key)) {
+                    return node.get(key).asText();
+                }
+                throw new IllegalArgumentException("no partition_key found in the source record");
+            })
+            .collect(Collectors.joining());
+
+        Integer partition = Utils.toPositive(Utils.murmur2(partitionKeyValue.getBytes())) % targetPartitions;
+
+        return partition;
     }
 
     @java.lang.Override
--- src/test/java/com/birdie/kafka/connect/smt/OutboxTest.java
+++ src/test/java/com/birdie/kafka/connect/smt/OutboxTest.java
@@ -4,6 +4,8 @@ import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.source.SourceRecord;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -12,9 +14,21 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class OutboxTest {
+    private Outbox transformer;
+
+    @Before
+    public void before(){
+        transformer = new Outbox();
+    }
+
+
+    @After
+    public void after(){
+        transformer.close();
+    }
+
     @Test
     public void sendsAMessageToTheCorrectTopicPartition() {
-        Outbox transformer = new Outbox();
         transformer.configure(new HashMap<>() {{
             put("topic", "caregivers.matches.v1");
         }});
@@ -31,6 +45,55 @@ public class OutboxTest {
         value.put("key", "1234");
         value.put("partition_number", 1);
         value.put("payload", "[\"foo\", \"bar\"]");
+
+        SourceRecord record = new SourceRecord(
+                // Where it comes from
+                null, null,
+                // Where it is going
+                "a-database-name.public.the_database_table", null,
+                // Key
+                SchemaBuilder.bytes().optional().build(),
+                "1234".getBytes(),
+                // Value
+                schema,
+                value
+        );
+
+        final SourceRecord transformedRecord = transformer.apply(record);
+
+        assertEquals("caregivers.matches.v1", transformedRecord.topic());
+        assertNotNull(transformedRecord.kafkaPartition());
+        assertEquals("1", transformedRecord.kafkaPartition().toString());
+    }
+
+    @Test
+    public void generatesPartitionNumber() {
+        transformer.configure(new HashMap<>() {{
+            put("topic", "caregivers.matches.v1");
+            put("auto-partitioning", true);
+
+            // TODO: test missing config
+            // TODO: test with 1 partition is always the same
+
+            put("num-target-partitions", 3);
+        }});
+
+        Schema schema = SchemaBuilder.struct()
+                .name("Value")
+                .field("key", SchemaBuilder.STRING_SCHEMA)
+                // Later: `partition_key`, SMT will figure out the number of partition of the target topic by itself
+                // .field("partition_number", SchemaBuilder.INT32_SCHEMA) // TODO test failure message when partition_number is set instead of partition_key
+                .field("partition_key", SchemaBuilder.STRING_SCHEMA)
+                .field("payload", SchemaBuilder.string().name("io.debezium.data.Json").optional().build())
+            .build();
+
+        Struct value = new Struct(schema);
+        value.put("key", "1234");
+        value.put("partition_key", "field2");
+        value.put("payload", "{\n" +
+                "  \"field1\": [{\"id\": 1}],\n" +
+                "  \"field2\": \"some-partition-key-value\"" +
+                "}");
 
         SourceRecord record = new SourceRecord(
                 // Where it comes from
